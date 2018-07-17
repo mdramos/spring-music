f99b99aa1ae8268e8348c955e27a06d0d90e28ac
Michael Ramos <mdramos8@gmail.com>
2018-07-17 08:50:50 -0400

    make output outside

--- build-main.sh
+++ build-main.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 cd app
 ./gradlew clean assemble
-mkdir build-output1
-cp build/libs/app-1.0.jar build-output1/app.jar
-
-pwd
-
+mkdir ../build-output
+cp build/libs/app-1.0.jar ../build-output1/app.jar
