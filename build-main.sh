cc86da9be4f5a8fdaca6a15522f4fdf5e8dddfa2
Michael Ramos <mdramos8@gmail.com>
2018-07-17 08:46:28 -0400

    test

--- build-main.sh
+++ build-main.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd app
 ./gradlew clean assemble
-mkdir build-output
-cp build/libs/app-1.0.jar build-output/app.jar
+mkdir build-output1
+cp build/libs/app-1.0.jar build-output1/app.jar
 
 pwd
 
